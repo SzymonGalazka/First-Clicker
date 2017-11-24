59669552327f4d2a00009ec7e884eaec4b2dafe9
SzymonGalazka <szymongalazka96@gmail.com>
2017-11-24 21:45:24 +0100

    Initial commit

---
+++ android/src/com/pl/firstclicker/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.pl.firstclicker;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.pl.firstclicker.FirstClickerGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FirstClickerGame(), config);
+	}
+}
