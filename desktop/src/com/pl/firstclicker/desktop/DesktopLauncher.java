7323db906e7dc3f0c55a9819f1c5746a27a9029d
SzymonGalazka <szymongalazka96@gmail.com>
2017-11-24 23:27:18 +0100

    config desktop launcher

--- desktop/src/com/pl/firstclicker/desktop/DesktopLauncher.java
+++ desktop/src/com/pl/firstclicker/desktop/DesktopLauncher.java
@@ -7,6 +7,12 @@ import com.pl.firstclicker.FirstClickerGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = FirstClickerGame.GAME_NAME;
+		config.width = FirstClickerGame.WIDTH;
+		config.height = FirstClickerGame.HEIGHT;
+		config.resizable = false;
+
 		new LwjglApplication(new FirstClickerGame(), config);
 	}
 }
