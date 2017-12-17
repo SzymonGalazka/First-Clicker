483374cb77feec821e36ede3ec9e84854d76b56f
SzymonGalazka <szymongalazka96@gmail.com>
2017-12-17 15:42:02 +0100

    Displaying Player on GameplayScreen

--- core/src/screens/GameplayScreen.java
+++ core/src/screens/GameplayScreen.java
@@ -3,14 +3,35 @@ package screens;
 
 import com.pl.firstclicker.FirstClickerGame;
 
+import entities.Player;
+
 public class GameplayScreen extends AbstractScreen{
+
+    private Player player;
+
     public GameplayScreen(FirstClickerGame game) {
         super(game);
         init();
     }
 
     public void init(){
-
+        initPlayer();
     }
 
+    private void initPlayer() {
+        player = new Player();
+        stage.addActor(player);
+    }
+    @Override
+    public void render(float delta){
+        super.render(delta);
+        update();
+        spriteBatch.begin();
+        stage.draw();
+        spriteBatch.end();
+    }
+
+    private void update() {
+        stage.act();
+    }
 }
