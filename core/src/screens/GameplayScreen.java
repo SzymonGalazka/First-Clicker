32367d4d26d8f35754f2e0370c6e825de3f58185
SzymonGalazka <szymongalazka96@gmail.com>
2017-12-17 15:05:44 +0100

    Added dummy gameplayscreen

---
+++ core/src/screens/GameplayScreen.java
@@ -0,0 +1,16 @@
+package screens;
+
+
+import com.pl.firstclicker.FirstClickerGame;
+
+public class GameplayScreen extends AbstractScreen{
+    public GameplayScreen(FirstClickerGame game) {
+        super(game);
+        init();
+    }
+
+    public void init(){
+         
+    }
+
+}
